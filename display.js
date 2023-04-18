debe83a927d8cbe7350bf4aa731b75d11beead0a
MichaelH616 <michaelhuang6160@gmail.com>
2023-04-18 15:33:30 -0700

    first commit

---
+++ display.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SLL{
+    constructor(head = null) {
+        this.head = head
+    }
+    addFront(val){
+        let newNode = new Node(val)
+        newNode.next = this.head
+        this.head = newNode
+        return this
+    }
+    display(){
+        if (!this.head){
+            return null
+        } else {
+            let SLLDisplay = ""
+            let runner = this.head
+            while(runner) {
+                SLLDisplay += runner.val
+                if (runner.next){
+                    SLLDisplay += ", "
+                }
+                runner = runner.next
+            }
+            return SLLDisplay
+        }
+        
+    }
+}
+
+let SL = new SLL()
+// console.log(SL.addFront(76).addFront(2).addFront(11.41))
+console.log(SL.display())
